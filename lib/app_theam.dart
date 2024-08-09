62eb0f90f708cd42f99d5a0d641e17f537ab7217
shubhamgitvns <shubhammaurya10092002@gmail.com>
2024-08-09 12:58:53 +0530

    Create introduction page of to-do app

---
+++ lib/app_theam.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class App_Text{
+  static final TextStyle button_text =
+  TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25);
+
+
+  static final Color button_color= Colors.teal;
+}
---
+++ lib/intro_page/inro_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/app_theam.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: 100,),
+            SizedBox(
+              height: 300,
+                child: Image.asset("images/intro.png")),
+
+            const SizedBox(height: 20,),
+            Text(
+              'Get Organized Your Life',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20,),
+            const SizedBox(
+              width: 300,
+              child: Text(
+                'This is a simple and effective to-do list and task manager app which helps you manage time.',
+                style: TextStyle(fontSize: 18,color: Colors.grey,fontWeight: FontWeight.w400),
+              ),
+            ),
+            const SizedBox(height: 100,),
+            Container(
+              height: 50,
+              width: 300,
+              color: App_Text.button_color,
+              child:  Center(child: Text("Get Started",
+              style: App_Text.button_text,),),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
